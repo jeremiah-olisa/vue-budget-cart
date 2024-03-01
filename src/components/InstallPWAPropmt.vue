ee132736522e362a189779c935fd7a0b3ff973cf
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2024-03-01 09:26:40 +0100

    pwa install modal

---
+++ src/components/InstallPWAPropmt.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  // AlertDialogTrigger
+} from './ui/alert-dialog'
+import { ref } from 'vue'
+
+import Cookies from "js-cookie"
+import { useToast } from './ui/toast';
+import { onMounted } from 'vue'
+
+const { toast } = useToast();
+const installPromptEvent = ref<BeforeInstallPromptEvent | null>(null)
+const isModalOpen = ref(false)
+
+
+const dismiss = () => {
+  Cookies.set("add-to-home-screen", 'NOT INSTALLED', { expires: 15 });
+  installPromptEvent.value = null;
+  isModalOpen.value = false;
+}
+
+const install = () => {
+  if (installPromptEvent.value) {
+    isModalOpen.value = false;
+    return installPromptEvent.value.prompt()
+  };
+
+  toast({ description: 'PWA prompt not initialised', variant: 'destructive' });
+}
+
+onMounted(() => {
+  // Event listener for beforeinstallprompt event
+  window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the default behavior
+    event.preventDefault()
+
+    if (Cookies.get("add-to-home-screen") == undefined) {
+      installPromptEvent.value = event as BeforeInstallPromptEvent;
+      isModalOpen.value = true;
+    }
+
+    console.log({ event, Cookies: Cookies.get("add-to-home-screen"), installPromptEvent: installPromptEvent.value })
+    // Store the event for later use
+  })
+})
+</script>
+
+<template>
+  <AlertDialog v-model:open="isModalOpen">
+    <!-- <AlertDialogTrigger>Open</AlertDialogTrigger> -->
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Install Tracka</AlertDialogTitle>
+        <AlertDialogDescription>
+          Add Tracka to your home screen for easy access and an enhanced experience. Enjoy faster load times, offline access, and more by installing now!
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel variant="destructive" @click="dismiss()">Dismiss</AlertDialogCancel>
+        <AlertDialogAction @click="install()">Install</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import AddItemModal from '@/components/AddItemModal.vue'
 import CartItemTable from '@/components/CartItemTable.vue'
+import InstallPWAPropmt from '@/components/InstallPWAPropmt.vue';
 import SetBudgetForm from '@/components/SetBudgetForm.vue'
 </script>
 
 <template>
   <div>
+    <InstallPWAPropmt />
+
     <SetBudgetForm />
     <!-- Top part: Budget setting -->
     <CartItemTable />
